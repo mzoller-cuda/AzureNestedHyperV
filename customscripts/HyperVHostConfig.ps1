e7a86018deef1605ba3b414b5491831ef42fc988
mzoller-cuda <mzoller@barracuda.com>
2020-07-18 18:08:59 +0200

    additional NICS and switches

--- customscripts/HyperVHostConfig.ps1
+++ customscripts/HyperVHostConfig.ps1
@@ -48,23 +48,39 @@ Configuration Main
 			SetScript =
 			{
 				[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+				# this is the switch that needs to be on P4 
 				$NatSwitch = Get-NetAdapter -Name "vEthernet (NAT Switch)"
 				New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex $NatSwitch.ifIndex
 				New-NetNat -Name NestedVMNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24 -Verbose
+				
+				# Create the rest of the switches 
+				New-VMSwitch "P1Switch" -SwitchType "Private" 
+				New-VMSwitch "P2Switch" -SwitchType "Private" 
+				New-VMSwitch "P3Switch" -SwitchType "Private" 
+				New-VMSwitch "P5Switch" -SwitchType "Private" 
+
 				$VHDDownload = "https://d.barracuda.com/ngfirewall/8.1.0/GWAY-8.1.0-0440-HyperV-VTxxx.vhd"
 				$downloadedFile = "D:\VTxxx.vhd"
 				$vmFolder = "C:\VM"
 				(New-Object System.Net.WebClient).DownloadFile($VHDDownload, $downloadedFile)
-#				Invoke-WebRequest -Uri $VHDDownload -OutFile $downloadedFile
-#				Add-Type -assembly "system.io.compression.filesystem"
-#				[io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
+
+
 				New-VM -Name CGWAN `
 					   -MemoryStartupBytes 2GB `
 					   -BootDevice VHD `
 					   -VHDPath 'D:\VTxxx.vhd' `
                       -Path 'C:\VM\' `
 					   -Generation 1 `
-				       -Switch "NAT Switch"
+#				       -Switch "NAT Switch"
+					   -Switch "P1Switch"
+					   
+				# Add network interfaces 
+				# note the order of the interfaces (they are jumbled)
+				Add-VMNetworkAdapter -Name "P2" -VMName CGWAN -SwitchName "NAT Switch"
+				Add-VMNetworkAdapter -Name "P3" -VMName CGWAN -SwitchName "P3Switch"
+				Add-VMNetworkAdapter -Name "P4" -VMName CGWAN -SwitchName "P2Switch"
+				Add-VMNetworkAdapter -Name "P5" -VMName CGWAN -SwitchName "P5Switch"
+
 			    Start-VM -Name CGWAN
 			}
 		}	
